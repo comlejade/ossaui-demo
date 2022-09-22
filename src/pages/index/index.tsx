da38963b0ffb34fd429e01cc62aaf4b334c477c2
comlejade <thundersmile@126.com>
2022-09-22 12:11:12 +0800

    add os input

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -6,9 +6,19 @@ import './index.scss'
 
 export default () => {
   const [concat, setConcat] = useState('')
+  const [v1, setV1] = useState("186");
   return (
     <View>
       <OsInput label='收货人' placeholder='请填写收货人姓名' value={concat} onChange={setConcat} />
+      <OsInput
+        label='标题'
+        placeholder='默认文案'
+        placeholderStyle='color: #7f7f7f;'
+        value={v1}
+        onChange={(v) => {
+          setV1(v);
+        }}
+      ></OsInput>
     </View>
   )
 }
